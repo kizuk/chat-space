f5c40d458e39c5ce356a0ce6f344aac8ef19fa1e
Negishi Kazuki <h2dhe2yc@icloud.com>
2018-10-31 19:59:52 +0900

    edit message.js
    返ってきたjsonをdoneメソッドで受け取り、htmlを作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,25 @@
 $(function(){
+
+ function buildHTML(message){
+ 	var html = `<div class="body__message-list">
+                 <div class="body__messages">
+                  <div class="body__messages-name">
+                   ${message.user.name}
+                  </div>
+                  <div class="body__message-time">
+                   ${message.created_at}
+                  </div>
+                  <div class="body__message-text">
+                   <p class="body__message-text_content">
+                    ${message.content}
+                   </p>
+                   <img src="${message.image.url}" class="lower-message__image"
+                  </div>
+                 </div>
+                </div>`
+    return html;
+ }
+
   $('.new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,5 +31,10 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data)
+      $('.messages').append(html)
+      $('.message__input').val('')
+    })
   })
-})
+});
