3ac7d701069ddec4c1215021977a4095be2945eb
Negishi Kazuki <h2dhe2yc@icloud.com>
2018-11-03 12:44:59 +0900

    aa

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -36,7 +36,7 @@ $(function() {
   });
 
   function addUser(name,id){
-   var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
                 <input name='group[user_ids][]' type='hidden' value="${id}">
                 <p class='chat-group-user__name'>${name}</p>
                 <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
